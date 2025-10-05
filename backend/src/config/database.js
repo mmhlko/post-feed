e14eaa0ae1b1043f4f3d60ccdf02c526773377a3
mmhlko <maximmhl@mail.ru>
2025-10-05 18:03:20 +0700

    add docker files

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -18,3 +18,13 @@ module.exports = {
     migrationStorageTableName: 'migrations',
   },
 };
+
+console.log({
+  dialect: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_NAME,
+  migrationStorageTableName: 'migrations',
+});
